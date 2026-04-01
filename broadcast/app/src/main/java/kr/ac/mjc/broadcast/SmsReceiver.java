dfbff565405c5283a65670a5208b536e2624f93b
jinyonghwa <jinyonghwa@kakao.com>
2026-04-01 20:23:07 +0900

    broadcast

---
+++ broadcast/app/src/main/java/kr/ac/mjc/broadcast/SmsReceiver.java
@@ -0,0 +1,20 @@
+package kr.ac.mjc.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+            SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+            if (messages != null && messages.length > 0) {
+                Toast.makeText(context, "문자가 왔습니다", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
